c0effe948bfe629c2bbcd73e2b4c9c2e4f56f676
Tuncay Kaplan <trkaplan@gmail.com>
2019-05-08 03:08:02 +0300

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+      </div>
+    );
+  }
+}
+export default App;
