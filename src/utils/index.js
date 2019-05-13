2f1f00add1d2463858c1d99fcf0bb7fc4243121b
Tuncay Kaplan <trkaplan@gmail.com>
2019-05-13 04:45:49 +0300

    update currency format locale witl "nl"

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 const formatCurrency = amount => {
-  const formatter = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('nl', {
     style: 'currency',
     currency: 'EUR'
   });
